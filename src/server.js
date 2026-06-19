d449f940771e1c5ec900d953c1a317dd8cff4b2e
Jay bhanushali <102736330+Jaybhanu12@users.noreply.github.com>
2026-06-19 23:26:30 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const bookingRoutes = require('./routes/bookings');
 const adminRoutes = require('./routes/admin'); 
 
 const app = express();
-
+app.set('trust proxy', 1);
 connectDB();
 
 app.use(
